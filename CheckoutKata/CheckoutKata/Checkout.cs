b8307856a324fe4a850d58d25eae7939c9271a01
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2019-04-07 17:54:05 +0100

    refactoring checkout to use a dictionary to store skus and prices so that we dont have to rely on conditionals for each sku

--- CheckoutKata/CheckoutKata/Checkout.cs
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -1,27 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CheckoutKata
 {
     public class Checkout
     {
         private int _total;
+        private Dictionary<string, int> _prices = new Dictionary<string, int> { { "A", 50 }, { "B", 30 }, { "C", 20 }, { "D", 15 } };
         public void Scan(string sku)
         {
-            switch (sku)
-            {
-                case "A":
-                    _total += 50;
-                    break;
-                case "B":
-                    _total += 30;
-                    break;
-                case "C":
-                    _total += 20;
-                    break;
-                case "D":
-                    _total += 15;
-                    break;
-            }
+            _total += _prices[sku];
         }
 
         public int GetTotalPrice()
