4aa6ff0cbae4d7cedfaea5ecf3228fe4d3f6b891
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2019-04-07 18:01:31 +0100

    Adding variable to keep track of how many As have been scanned

--- CheckoutKata/CheckoutKata/Checkout.cs
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -6,14 +6,23 @@ namespace CheckoutKata
     public class Checkout
     {
         private int _total;
+        private int _numberOfAs;
         private Dictionary<string, int> _prices = new Dictionary<string, int> { { "A", 50 }, { "B", 30 }, { "C", 20 }, { "D", 15 } };
         public void Scan(string sku)
         {
             _total += _prices[sku];
+            if (sku == "A")
+            {
+                _numberOfAs++;
+            }
         }
 
         public int GetTotalPrice()
         {
+            if (_numberOfAs == 3)
+            {
+                _total -= 20;
+            }
             return _total;
         }
     }
