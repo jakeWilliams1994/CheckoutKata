8e549673ff5268c986a58577f2c59ec5721a36c5
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2019-04-07 18:18:42 +0100

    Removing some conditional code so that counting the number of times a sku is scanned should work with any sku

--- CheckoutKata/CheckoutKata/Checkout.cs
+++ CheckoutKata/CheckoutKata/Checkout.cs
@@ -7,27 +7,26 @@ namespace CheckoutKata
     {
         private int _total;
         private Dictionary<string, int> _prices = new Dictionary<string, int> { { "A", 50 }, { "B", 30 }, { "C", 20 }, { "D", 15 } };
-        private Dictionary<string, int> _scanned = new Dictionary<string, int> { { "A", 0 }, { "B", 0 } };
+        private Dictionary<string, int> _scanned = new Dictionary<string, int>();
         public void Scan(string sku)
         {
             _total += _prices[sku];
-            if (sku == "A")
-            {
+            if (_scanned.ContainsKey(sku)) {
                 _scanned[sku]++;
             }
-            if (sku == "B")
+            else
             {
-                _scanned[sku]++;
+                _scanned.Add(sku, 1);
             }
         }
 
         public int GetTotalPrice()
         {
-            if (_scanned["A"] % 3 == 0)
+            if (_scanned.ContainsKey("A") &&_scanned["A"] % 3 == 0)
             {
                 _total -= (20 * (_scanned["A"] / 3));
             }
-            if (_scanned["B"] % 2 == 0)
+            if (_scanned.ContainsKey("B") && _scanned["B"] % 2 == 0)
             {
                 _total -= (15 * (_scanned["B"] / 2));
             }
